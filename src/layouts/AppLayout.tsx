031293b860e1800e398125ed0767ed8ed198d328
Jorge-Rmz <jorgre5675420j@gmail.com>
2025-06-21 16:42:05 -0600

    Creacion de el Layout

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Logo from '../componets/Logo'
 export default function AppLayout() {
-  return (
-    <div>
-      <Outlet/>
-    </div>
-  )
+    return (
+        <div>
+            <header className='bg-gray-800 py-5'>
+                <div className='max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center'>
+                    <div className='w-64 '>
+                        <Logo />
+                    </div>
+                </div>
+            </header>
+
+            <section className='max-w-screen-2xl mx-auto mt-10 p-5'>
+
+                <Outlet />
+            </section>
+
+            <footer className='py-5'>
+                <p className='text-center'>
+                    Todos los derechos reservados &copy; {new Date().getFullYear()} - UpTask
+                </p>
+            </footer>
+        </div>
+    )
 }
--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -1,17 +1,11 @@
 import React from 'react'
+import Logo from '../componets/Logo'
 
 export default function DashboardView() {
   return (
     <div>
-        <h1 className="text-3xl font-bold underline">
-            Dashboard View
-        </h1>
-        <p className="mt-4 text-lg">
-            Welcome to the dashboard! Here you can find various insights and analytics.
-        </p>
-        <div className="mt-6">
-            {/* Add more dashboard components or widgets here */}
-        </div>
+        
+
     </div>
   )
 }
