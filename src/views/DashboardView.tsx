85e0d4ba4ce4456bff26f54970fa0b5b61b1db53
Jorge-Rmz <jorgre5675420j@gmail.com>
2025-06-21 16:07:23 -0600

    first commit

---
+++ src/views/DashboardView.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function DashboardView() {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold underline">
+            Dashboard View
+        </h1>
+        <p className="mt-4 text-lg">
+            Welcome to the dashboard! Here you can find various insights and analytics.
+        </p>
+        <div className="mt-6">
+            {/* Add more dashboard components or widgets here */}
+        </div>
+    </div>
+  )
+}
